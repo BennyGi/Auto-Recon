a898aa52ee10d83d6234dba3f4073706afc9fb53
BennyGi <bennygorno@gmail.com>
2025-11-20 03:18:14 -0500

    Added Whois scan and saving the output

--- AutoRecon.py
+++ AutoRecon.py
@@ -1,4 +1,10 @@
 import sys
+import subprocess
+import os
+
+def run_cmd(cmd):
+    result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+    return result.stdout
 
 def main():
     if len(sys.argv) < 2:
@@ -7,6 +13,19 @@ def main():
     
     domain = sys.argv[1]
     print(f"[+] Target: {domain}")
+
+    outdir = f"results/{domain}"
+    os.makedirs(outdir, exist_ok=True)
+
+    # WHOIS 
+    print("[+] Running WHOIS...")
+    whois_output = run_cmd(f"whois {domain}")
+
+    # Saving the file
+    with open(f"{outdir}/whois.txt", "w") as f:
+        f.write(whois_output)
+
+    print("[+] WHOIS saved to results/" + domain + "/whois.txt")
 
 if __name__ == "__main__":
     main()
