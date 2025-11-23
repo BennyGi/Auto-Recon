c667d93c0cc7d70960ad97ff8d7e76eb1a12ff73
BennyGi <bennygorno@gmail.com>
2025-11-23 06:32:02 -0500

    Added reporting system (technical + human summary).

--- AutoRecon.py
+++ AutoRecon.py
@@ -17,38 +17,47 @@ def main():
     outdir = f"results/{domain}"
     os.makedirs(outdir, exist_ok=True)
 
-    # WHOIS 
+    # ---------- WHOIS ----------
     print("[+] Running WHOIS...")
     whois_output = run_cmd(f"whois {domain}")
 
-    # Saving the file
     with open(f"{outdir}/whois.txt", "w") as f:
         f.write(whois_output)
 
     print("[+] WHOIS saved to results/" + domain + "/whois.txt")
     
 
-    # DNS/DIG
+    # ---------- DIG / DNS ----------
     print("[+] Running DIG...")
-    dns_output = run_cmd(f"dig{domain} ANY+noidnout")
+    dns_output = run_cmd(f"dig {domain} ANY +noidnout")
 
-      # Saving the file
-    with open(f"{outdir}/dns.txt, "w") as f:
+    with open(f"{outdir}/dns.txt", "w") as f:
         f.write(dns_output)
-    print("[+] DNS saved to results.")
+
+    print("[+] DNS saved to results/" + domain + "/dns.txt")
 
 
-    # NMAP
+    # ---------- NMAP ----------
     print("[+] Running NMAP...")
     nmap_output = run_cmd(f"nmap -sV --min-rate 500 {domain}")
 
-    
-    # Saving the file
     with open(f"{outdir}/nmap.txt", "w") as f:
         f.write(nmap_output)
 
     print("[+] NMAP saved to results/" + domain + "/nmap.txt")
 
+
+    # ---------- FFUF ----------
+    print("[+] Running FFUF...")
+
+    wordlist = "/usr/share/wordlists/dirb/common.txt"
+    ffuf_cmd = f"ffuf -u http://{domain}/FUZZ -w {wordlist} -mc 200,301,302 -t 40"
+    ffuf_output = run_cmd(ffuf_cmd)
+    
+    with open(f"{outdir}/ffuf.txt", "w") as f:
+        f.write(ffuf_output)
+
+    print("[+] FFUF saved to results/" + domain + "/ffuf.txt")
 
 if __name__ == "__main__":
     main()
